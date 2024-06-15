e0bfd5fdc997a4825b6afbe0a1fb351d320f12a7
sapir703 <sapirsalhov@gmail.com>
2024-06-15 21:37:34 +0300

    Add files via upload

---
+++ php/db_connection.php
@@ -0,0 +1,8 @@
+<?php
+$hostname = "localhost";
+$username = "sapirsv_db_user";
+$password = "sapir123456789123";
+$database = "sapirsv_db";
+
+$connection = mysqli_connect($hostname, $username, $password, $database);
+?>
